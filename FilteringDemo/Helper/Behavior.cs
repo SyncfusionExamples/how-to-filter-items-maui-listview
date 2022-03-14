0b3e5ec4f0c7797360ccfac1de6b85646dbbc2a0
RiyashameedM <97871624+RiyashameedM@users.noreply.github.com>
2022-03-14 18:23:51 +0530

    Add files via upload

---
+++ FilteringDemo/Helper/Behavior.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Maui.ListView;
+using Syncfusion.Maui.DataSource;
+
+namespace FilteringDemo
+{
+    public class Behavior : Behavior<ContentPage>
+    {
+        #region Fields
+
+        SfListView ListView;
+        SearchBar SearchBar;
+        #endregion
+
+        #region Overrides
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            ListView = bindable.FindByName<SfListView>("listView");
+            SearchBar = bindable.FindByName<SearchBar>("filterText");
+            SearchBar.TextChanged += SearchBar_TextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            SearchBar.TextChanged -= SearchBar_TextChanged;
+            SearchBar = null;
+            ListView = null;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+
+        #region Methods
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (ListView.DataSource != null)
+            {
+                ListView.DataSource.Filter = FilterContacts;
+                ListView.DataSource.RefreshFilter();
+            }
+            ListView.RefreshView();
+        }
+
+        private bool FilterContacts(object obj)
+        {
+            if (SearchBar == null || SearchBar.Text == null)
+            return true;
+            var taskInfo = obj as TaskInfo;
+            return (taskInfo.Title.ToLower().Contains(SearchBar.Text.ToLower())
+                || taskInfo.Description.ToLower().Contains(SearchBar.Text.ToLower()));
+        }
+        #endregion
+
+    }
+}
---
+++ FilteringDemo/ViewModel/MainViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteringDemo
+{
+    public class MainViewModel 
+    {
+        #region Properties
+        public ObservableCollection<TaskInfo> Items { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            AddItemDetails();
+        }
+        #endregion
+
+        #region Methods
+        public void AddItemDetails()
+        {
+            Items = new ObservableCollection<TaskInfo>();
+            var random = new Random();
+
+            for (int i = 0; i < Features.Count(); i++)
+            {
+                var details = new TaskInfo()
+                {
+                    Title = Features[i],
+                    Description = DescriptionList[i],
+                    Tag = Tags[random.Next(0, 4)],
+                };
+                Items.Add(details);
+            }
+
+        }
+        #endregion
+
+        #region Field
+
+        string[] Tags = new string[]
+        {
+            "Feature Implementation",
+            "Bug Fixing",
+            "Testing",
+            "Design",
+            "Post Processing"
+        };
+
+        string[] Features = new string[]
+        {
+            "Drag and drop",
+            "Swiping",
+            "Pull To Refresh",
+            "Selection in row header",
+            "Multiple selection color",
+            "Animating the selected row",
+            "Long press event",
+
+        };
+
+        string[] DescriptionList = new string[] {
+            "Rearrange the columns by dragging and dropping them",
+            "Enables the users to swipe",
+            "Pull To Refresh action refreshes the grid",
+            "Apply selection using row header",
+            "Apply different selection colors for different rows",
+            "Add an animation upon selecting a row",
+            "Users can listen to LongPresses in the listview",
+
+        };
+        #endregion
+    }
+}
