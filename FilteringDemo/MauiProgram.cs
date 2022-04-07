53db7d9c1e364a9ddbe4b215366b71c11af56072
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2022-04-07 12:59:48 +0530

    updated majuiprogram.cs

--- FilteringDemo/MauiProgram.cs
+++ FilteringDemo/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿namespace FilteringDemo;
+﻿using Syncfusion.Maui.ListView.Hosting;
+namespace FilteringDemo;
 
 public static class MauiProgram
 {
@@ -13,7 +14,7 @@ public static class MauiProgram
 				fonts.AddFont("Roboto-Medium.ttf", "RobotoMedium");
 				fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
 			});
-
+		builder.ConfigureSyncfusionListView();
 		return builder.Build();
 	}
 }
